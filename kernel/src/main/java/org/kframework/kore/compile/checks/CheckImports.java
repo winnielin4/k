e78ac007270012073b1760b9893da2d9f26fbd01
Dwight Guth <dwightguth@runtimeverification.com>
2016-08-04 10:42:48 -0500

    interview skeleton

---
+++ kernel/src/main/java/org/kframework/kore/compile/checks/CheckImports.java
@@ -0,0 +1,18 @@
+package org.kframework.kore.compile.checks;
+
+import org.kframework.definition.Module;
+import org.kframework.utils.errorsystem.KExceptionManager;
+
+public class CheckImports {
+    private final KExceptionManager kem;
+
+    public CheckImports(KExceptionManager kem) {
+        this.kem = kem;
+    }
+
+    public void check(Module m) {
+        if (false) {
+            kem.registerCompilerWarning("Error message");
+        }
+    }
+}
